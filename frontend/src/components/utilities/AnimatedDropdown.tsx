8bb9cb936a23fcf32e4f8e4c78110b5b7dcd4f58
Dreadhalor <Scotthetrick2@yahoo.com>
2022-11-25 13:47:09 -0800

    Removed using AnimatedDropdown as a forwardRef Changed dropdown speed to 200ms

--- frontend/src/components/utilities/AnimatedDropdown.tsx
+++ frontend/src/components/utilities/AnimatedDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 type Props = {
   isOpen: boolean;
@@ -9,53 +9,52 @@ type Props = {
 };
 
 // this only animates to an explicit height, not to auto
-const AnimatedDropdown = forwardRef(
-  (
-    { maxHeight, isOpen, border, children, dragOutline = false }: Props,
-    ref: any
-  ) => {
-    const [dragover, setDragover] = useState(false);
+const AnimatedDropdown = ({
+  maxHeight,
+  isOpen,
+  border,
+  children,
+  dragOutline = false,
+}: Props) => {
+  const [dragover, setDragover] = useState(false);
 
-    const default_max_height = '500px';
-    const max_height = maxHeight ?? default_max_height;
+  const default_max_height = '500px';
+  const max_height = maxHeight ?? default_max_height;
 
-    const size_ref = useRef<HTMLDivElement>(null);
-    return (
-      <div
-        ref={ref}
-        className='relative cursor-default overflow-hidden transition-all duration-300'
-        style={{ maxHeight: isOpen ? max_height : '0' }}
-      >
-        {dragOutline && (
-          <div
-            className='pointer-events-none absolute inset-[1px] z-10'
-            style={{ border: dragover ? '2px dashed blue' : 'none' }}
-          ></div>
-        )}
+  const size_ref = useRef<HTMLDivElement>(null);
+  return (
+    <div
+      className='relative cursor-default overflow-hidden transition-all duration-200'
+      style={{ maxHeight: isOpen ? max_height : '0' }}
+    >
+      {dragOutline && (
         <div
-          ref={size_ref}
-          className={`overflow-auto ${border}`}
-          style={{
-            maxHeight: max_height,
-          }}
-          onDragOver={(e) => {
-            e.preventDefault();
-            setDragover(true);
-          }}
-          onDragLeave={(e) => {
-            e.preventDefault();
-            setDragover(false);
-          }}
-          onDrop={(e) => {
-            e.preventDefault();
-            setDragover(false);
-          }}
-        >
-          {children}
-        </div>
+          className='pointer-events-none absolute inset-[1px] z-10'
+          style={{ border: dragover ? '2px dashed blue' : 'none' }}
+        ></div>
+      )}
+      <div
+        ref={size_ref}
+        className={`overflow-auto ${border}`}
+        style={{
+          maxHeight: max_height,
+        }}
+        onDragOver={(e) => {
+          e.preventDefault();
+          setDragover(true);
+        }}
+        onDragLeave={(e) => {
+          e.preventDefault();
+          setDragover(false);
+        }}
+        onDrop={(e) => {
+          e.preventDefault();
+          setDragover(false);
+        }}
+      >
+        {children}
       </div>
-    );
-  }
-);
-
+    </div>
+  );
+};
 export default AnimatedDropdown;
