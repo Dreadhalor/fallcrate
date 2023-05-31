cc73d422c303519fef3af593458aebbccad825c9
Dreadhalor <Scotthetrick2@yahoo.com>
2023-05-30 19:27:23 -1000

    Resets current directory to root on uid change

--- frontend/src/hooks/fileserver/useCurrentDirectory.tsx
+++ frontend/src/hooks/fileserver/useCurrentDirectory.tsx
@@ -2,20 +2,26 @@ import { sortFiles } from '@src/helpers';
 import { CustomFile } from '@src/types';
 import { useState, useEffect } from 'react';
 import { useFiles } from './useFiles';
+import { useAuth } from 'milestone-components';
 
 // okay pretty sure the only reason stuff like this is kosher as a hook instead of a context is because it's
 // only instantiated once thru FilesystemProvider & then that instance is passed to every other component that needs it
 export const useCurrentDirectory = () => {
   const [currentDirectory, setCurrentDirectory] = useState<string | null>(null);
-  const [currentDirectoryFiles, setCurrentDirectoryFiles] = useState<CustomFile[]>([]);
+  const [currentDirectoryFiles, setCurrentDirectoryFiles] = useState<
+    CustomFile[]
+  >([]);
 
   // which also means useFiles is dangerous but since all instances use Firestore as the source of truth it's fine
   // FOR NOW
   const { files } = useFiles();
+  const { uid } = useAuth();
 
   useEffect(() => {
     openDirectory(currentDirectory);
   }, [files]);
+
+  useEffect(() => openDirectory(null), [uid]);
 
   const openDirectory = (directory_id: string | null) => {
     setCurrentDirectory(directory_id);
@@ -28,6 +34,6 @@ export const useCurrentDirectory = () => {
   return {
     currentDirectory,
     currentDirectoryFiles,
-    openDirectory
-  }
-}
+    openDirectory,
+  };
+};
